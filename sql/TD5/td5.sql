ec8b39e48b075780273e4b6abebbdec5b55cc997
alamchichi <ayoub.lamchichi.info@gmail.com>
2023-12-06 14:28:14 +0100

    td finito

--- sql/TD5/td5.sql
+++ sql/TD5/td5.sql
@@ -45,7 +45,7 @@ SET search_path TO lire;
         ROLLBACK;  -- Annule la transaction
 
         -- Session B
-        SELECT * FROM lecteur;  -- Les deux nouveaux lecteurs ne seront pas présents
+        SELECT * FROM lecteur;  -- Les deux nouveaux lecteurs ne seront pas présents dans les deux Sessions
 
 -- Question 7 --------------------------------------------------------------------------------------------------
 
@@ -60,10 +60,74 @@ SET search_path TO lire;
         -- Session A
         SELECT COUNT(*) FROM livre;  -- On en a 17
 
+
 -- Question 10 --------------------------------------------------------------------------------------------------
 
-        UPDATE lecteur SET nom ='toto' WHERE num_lecteur=1 ;
+                -- Session A
+                SELECT nom FROM lecteur WHERE num_lecteur = 1;  -- Affiche le nom du lecteur 1
 
-        COMMIT;
+                -- Session B
+                UPDATE lecteur SET nom ='toto' WHERE num_lecteur=1;  -- Modifie le nom du lecteur 1
+                COMMIT;  -- On valide
 
-        
+                -- Session A
+                SELECT nom FROM lecteur WHERE num_lecteur = 1;  -- Affiche de nouveau le nom du lecteur 1
+                -- On ne constate aucun changement car la session A est isolée (SERIALIZABLE)
+
+-- Question 11 --------------------------------------------------------------------------------------------------
+
+                -- Session A
+                COMMIT;  -- Fin de la transaction
+                SELECT nom FROM lecteur WHERE num_lecteur = 1;  -- Reconsulte le nom du lecteur 1
+                -- On constate le changement après le commit
+
+-- Question 12 --------------------------------------------------------------------------------------------------
+
+                -- Session A
+                SELECT COUNT(*) FROM lecteur;  -- Compte le nombre de lecteurs
+
+                -- Session B
+                INSERT INTO lecteur (nom) VALUES ('Mme Belabbes');
+                COMMIT;  -- Ajoute un lecteur et valide
+
+                -- Session A
+                SELECT COUNT(*) FROM lecteur;  -- Recompte le nombre de lecteurs
+                -- On ne constate pas le nouveau lecteur ajouté
+
+-- Question 13 --------------------------------------------------------------------------------------------------
+
+                -- Session A
+                COMMIT;  -- Fin de la transaction
+                SELECT COUNT(*) FROM lecteur;  -- Recompte le nombre de lecteurs
+                -- On constate maintenant le nouveau lecteur ajouté
+
+-- Question 14 --------------------------------------------------------------------------------------------------
+
+                -- La collection de "Risibles amours" reste "Folio" dans la session A
+
+-- Question 15 --------------------------------------------------------------------------------------------------
+
+                -- Le nombre de livres reste 15 dans la session A
+
+-- Question 16 --------------------------------------------------------------------------------------------------
+
+                -- Un "fantôme" fait référence à l'apparition de nouvelles lignes dans les résultats d'une requête au cours d'une même transaction
+
+-- Question 17 --------------------------------------------------------------------------------------------------
+
+                -- READ COMMITED permet de voir les modifications 'committées' pendant une transaction
+                -- SERIALIZABLE isole complètement la transaction des modifications extérieures
+
+-- Question 18 --------------------------------------------------------------------------------------------------
+
+                 -- Utiliser SERIALIZABLE dans des contextes où l'intégrité des données est cruciale et où il faut éviter toute interférence entre transactions
+
+-- Question 19 --------------------------------------------------------------------------------------------------
+
+                -- Tableau des niveaux d'isolation:
+                
+                -- Read committed: Lecture sale: Impossible, Lecture non reproductible: Possible, Lecture fantôme: Possible
+                
+                -- Serializable: Lecture sale: Impossible, Lecture non reproductible: Impossible, Lecture fantôme: Impossible
+
+-----------------------------------------------------------------------------------------------------------------
